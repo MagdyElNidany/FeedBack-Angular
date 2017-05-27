845ffbc980dd4635435637bbb6784c0042afd206
mostafamedhat94 <mostafa.medhat994@gmail.com>
2017-05-27 19:15:35 +0200

    progManager_accepts_invitation

--- src/index.route.js
+++ src/index.route.js
@@ -18,4 +18,19 @@ app
           url: '/admin/signout',
           templateUrl: 'app/views/admin/authentication/signout.html'
         });
+      $stateProvider
+        .state('prog_manager_invitation', {
+          url: '/admin/prog_managers/invite',
+          templateUrl: 'app/views/admin/prog_managers/invite.html'
+        });
+      $stateProvider
+        .state('prog_manager_accept_invitation', {
+          url: '/prog_manager/invitation/accept/:token',
+          templateUrl: 'app/views/prog_managers/accept_invitation.html'
+        });
+        $stateProvider
+          .state('prog_manager_submit_form', {
+            url: '/',
+            templateUrl: 'app/views/home/index.html'
+          })
       });
